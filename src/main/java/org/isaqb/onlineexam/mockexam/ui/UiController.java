6a9a3599948d126c6ee92923ed0573cf589b08e4
Jan MatÃšrne <jan@materne.de>
2021-11-14 14:20:46 +0100

    Prüfung als Sonderfall eines Quizzes (zumindest aus Sicht der Konfiguration)

--- src/main/java/org/isaqb/onlineexam/mockexam/ui/UiController.java
+++ src/main/java/org/isaqb/onlineexam/mockexam/ui/UiController.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.io.IOUtils;
 import org.isaqb.onlineexam.mockexam.BuildInfo;
-import org.isaqb.onlineexam.mockexam.QuizConfiguration;
+import org.isaqb.onlineexam.mockexam.DataConfiguration;
 import org.isaqb.onlineexam.mockexam.loader.AsciidocReader;
 import org.isaqb.onlineexam.mockexam.loader.IntroductionLoader;
 import org.isaqb.onlineexam.mockexam.model.Exam;
@@ -36,198 +36,200 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller 
+@Controller
 public class UiController {
-	
-	private Exam exam;
-	private IntroductionLoader introductionLoader;
-	private JsonMapper jsonMapper;
-	private I18NText cookieDislaimer;
-	private I18NText howToUse;
-	private AutloadJS autoloadJS;
-	private QuizConfiguration quizConfiguration;
-	
 
-	public UiController(
-			Exam exam, 
-			IntroductionLoader introductionLoader, 
-			JsonMapper jsonMapper,
-			AsciidocReader adocReader,
-			@Value("classpath:messages/cookie-disclaimer.adoc") Resource resourceCookieDisclaimer,
-			@Value("classpath:messages/how-to-use.adoc") Resource howToUse,
-			AutloadJS autloadJS,
-			QuizConfiguration quizConfiguration
-	) throws IOException {
-		this.exam = exam;
-		this.introductionLoader = introductionLoader;
-		this.jsonMapper = jsonMapper;
-		this.cookieDislaimer = parseADoc(adocReader, resourceCookieDisclaimer);
-		this.howToUse = parseADoc(adocReader, howToUse);
-		this.autoloadJS = autloadJS;
-		this.quizConfiguration = quizConfiguration;
-	}
+    private Exam exam;
+    private IntroductionLoader introductionLoader;
+    private JsonMapper jsonMapper;
+    private I18NText cookieDislaimer;
+    private I18NText howToUse;
+    private AutloadJS autoloadJS;
+    private DataConfiguration quizConfiguration;
 
+    public UiController(Exam exam, IntroductionLoader introductionLoader, JsonMapper jsonMapper,
+            AsciidocReader adocReader,
+            @Value("classpath:messages/cookie-disclaimer.adoc") Resource resourceCookieDisclaimer,
+            @Value("classpath:messages/how-to-use.adoc") Resource howToUse, AutloadJS autloadJS,
+            DataConfiguration quizConfiguration) throws IOException {
+        this.exam = exam;
+        this.introductionLoader = introductionLoader;
+        this.jsonMapper = jsonMapper;
+        this.cookieDislaimer = parseADoc(adocReader, resourceCookieDisclaimer);
+        this.howToUse = parseADoc(adocReader, howToUse);
+        this.autoloadJS = autloadJS;
+        this.quizConfiguration = quizConfiguration;
+    }
 
+    private I18NText parseADoc(AsciidocReader adocReader, Resource resourceCookieDisclaimer) throws IOException {
+        String adocCookieDisclaimer = IOUtils.toString(resourceCookieDisclaimer.getInputStream(),
+                Charset.defaultCharset());
+        return adocReader.parse(adocCookieDisclaimer);
+    }
 
-	private I18NText parseADoc(AsciidocReader adocReader, Resource resourceCookieDisclaimer) throws IOException {
-		String adocCookieDisclaimer = IOUtils.toString(resourceCookieDisclaimer.getInputStream(), Charset.defaultCharset());
-		return adocReader.parse(adocCookieDisclaimer);
-	}
+    @GetMapping("introduction.html")
+    public String introduction(HttpServletResponse response, Model model, @RequestParam("language") String language) {
+        response.addCookie(new Cookie("language", language));
+        Language lang = Language.valueOf(language);
 
+        model.addAttribute("html", introductionLoader.getHtml(lang));
+        model.addAttribute("cookieDisclaimer", cookieDislaimer.getText(lang));
+        model.addAttribute("howToUse", howToUse.getText(lang));
+        model.addAttribute("appversion",
+                String.format("Version %s - Build %s", BuildInfo.getVersion(), BuildInfo.getBuildTimestamp()));
+        model.addAttribute("quizOptions", possibleQuizOptions(lang));
+        autoloadJS.injectAutoReloadJS(model);
 
+        return "introduction.html";
+    }
 
-	@GetMapping("introduction.html")
-	public String introduction(HttpServletResponse response, Model model, @RequestParam("language") String language) {
-		response.addCookie(new Cookie("language", language));
-		Language lang = Language.valueOf(language);
-		
-		model.addAttribute("html", introductionLoader.getHtml(lang));
-		model.addAttribute("cookieDisclaimer", cookieDislaimer.getText(lang));
-		model.addAttribute("howToUse", howToUse.getText(lang));
-		model.addAttribute("appversion", String.format("Version %s - Build %s", BuildInfo.getVersion(), BuildInfo.getBuildTimestamp()));
-		model.addAttribute("quizOptions", possibleQuizOptions(lang));
-		autoloadJS.injectAutoReloadJS(model);
-		
-		return "introduction.html";
-	}
+    private List<QuizOptions> possibleQuizOptions(Language lang) {
+        return quizConfiguration.getTasks().entrySet().stream()
+                .map(e -> new QuizOptions(e.getKey(), e.getValue().getName().get(lang))).toList();
+    }
 
-	private List<QuizOptions> possibleQuizOptions(Language lang) {
-		return quizConfiguration.getQuiz().entrySet().stream()
-			.map( e -> new QuizOptions(e.getKey(), e.getValue().getName().get(lang)) )
-			.toList();
-	}
+    @GetMapping("process-exam.html")
+    public String processExam(
+            Model model, 
+            @RequestParam(required = false) String quizmode,
+            @CookieValue(name = "language", required = false) String language,
+            @CookieValue(name = "questionIds", required = false) String questionIds,
+            @CookieValue(name = "givenAnswers", required = false) String givenAnswersJson 
+    ) {
+        List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJson);
+        Exam e = getExam(quizmode, questionIds);
 
+        UIData uiData = new UIData(exam, Language.valueOf(language), givenAnswers, null);
+        model.addAttribute("exam", exam);
+        model.addAttribute("util", uiData);
+        model.addAttribute("givenAnswers", givenAnswers);
+        model.addAttribute("language", language);
+        autoloadJS.injectAutoReloadJS(model);
 
+        return "process-exam.html";
+    }
 
-	@GetMapping("process-exam.html")
-	public String processExam(
-			Model model, 
-			@RequestParam String language,
-			@RequestParam String mode,
-			@CookieValue(name = "language", required = false) String languageFromCookie, 
-			@CookieValue(name = "quizmode", required = false) String quizmodeFromCookie,
-			@CookieValue(name = "givenAnswers", required = false) String givenAnswersJson
-	) {
-		String quizmode = cookieOverParameter(quizmodeFromCookie, mode);
-		String lang = cookieOverParameter(languageFromCookie, language);
-		List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJson);
+    private Exam getExam(String quizmode, String questionIds) {
+        System.out.println("getExam()");
+        System.out.printf("- mode: %s%n", quizmode);
+        System.out.printf("- ids: %s%n", questionIds);
+        if (questionIds != null) {
+            return getExamFromQuestionIds(questionIds);
+        } else {
+            return quizmode == null ? getMockExam() : getQuizExam(quizmode);
+        }
+    }
 
-		UIData uiData = new UIData(exam, Language.valueOf(languageFromCookie), givenAnswers, null);
-		model.addAttribute("exam", exam);
-		model.addAttribute("util", uiData);
-		model.addAttribute("givenAnswers", givenAnswers);
-		model.addAttribute("language", lang);
-		autoloadJS.injectAutoReloadJS(model);
-		
-		return "process-exam.html";
-	}
-	
-	private String cookieOverParameter(String cookieValue, String paramValue) {
-		return cookieValue != null ? cookieValue : paramValue;
-	}
-	
+    private Exam getExamFromQuestionIds(String questionIds) {
+        return null;
+    }
 
+    private Exam getMockExam() {
+        return exam;
+    }
 
-	private List<TaskAnswer> givenAnswersFromCookie(String givenAnswersJson) {
-		return givenAnswersJson == null
-			? Collections.emptyList()
-			: jsonMapper.fromStringToAnswers(givenAnswersJson);
-	}
-	 
-	
-	
-	@PostMapping(value="send-exam")
-	public String sendExam(HttpServletResponse response, Model model, @RequestBody(required =false) MultiValueMap<String, String> formData) {
-		Collection<TaskAnswer> givenAnswers = parse(formData);
-		model.addAttribute("givenAnswers", givenAnswers);
-		response.addCookie(new Cookie("givenAnswers", jsonMapper.toString(givenAnswers)));
-		autoloadJS.injectAutoReloadJS(model);
-		return answersMissing(givenAnswers) ? "missing-tasks.html" : "redirect:/calculatePoints";
-	}
+    private Exam getQuizExam(String quizmode) {
+        return null;
+    }
 
-	private boolean answersMissing(Collection<TaskAnswer> givenAnswers) {
-		return givenAnswers.stream().filter(a -> !a.getOptionSelections().isEmpty()).count() < exam.getTasks().size();
-	}
+    private String cookieOverParameter(String cookieValue, String paramValue) {
+        return cookieValue != null ? cookieValue : paramValue;
+    }
 
-	private Collection<TaskAnswer> parse(MultiValueMap<String, String> formData) {
-		if (formData == null) {
-			return Collections.emptyList();
-		}
-		
-		Map<Integer, TaskAnswer> answers = new HashMap<Integer, TaskAnswer>();
-		
-		for(Entry<String, List<String>> entry : formData.entrySet()) {
-			String[] parts = entry.getKey().split("-");
-			if ("frage".equals(parts[0])) {
-				int nr = Integer.parseInt(parts[1]);
-				String option = parts[2];
-				var answer = answers.computeIfAbsent(nr, x -> new TaskAnswer(x));
-				answer.put(option, entry.getValue());
-				
-			} 
-			if ("flag".equals(parts[0])) {
-				int nr = Integer.parseInt(parts[1]);
-				boolean flagged = entry.getValue().contains("on");
-				var answer = answers.computeIfAbsent(nr, x -> new TaskAnswer(x));
-				answer.setFlagged(flagged);
-			}
-		}
-		
-		return answers.values();
-	}
+    private List<TaskAnswer> givenAnswersFromCookie(String givenAnswersJson) {
+        return givenAnswersJson == null ? Collections.emptyList() : jsonMapper.fromStringToAnswers(givenAnswersJson);
+    }
 
-	
-	
-	@GetMapping("calculatePoints")
-	public String calculatePoints(HttpServletResponse response, Model model, @CookieValue("language") String language, @CookieValue(name = "givenAnswers", required = false) String givenAnswersJsonBase64) {
-		List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJsonBase64);
+    @PostMapping(value = "send-exam")
+    public String sendExam(HttpServletResponse response, Model model,
+            @RequestBody(required = false) MultiValueMap<String, String> formData) {
+        Collection<TaskAnswer> givenAnswers = parse(formData);
+        model.addAttribute("givenAnswers", givenAnswers);
+        response.addCookie(new Cookie("givenAnswers", jsonMapper.toString(givenAnswers)));
+        autoloadJS.injectAutoReloadJS(model);
+        return answersMissing(givenAnswers) ? "missing-tasks.html" : "redirect:/calculatePoints";
+    }
 
-		Calculator calc = new Calculator();
-		var result = calc.calculate(exam, givenAnswers);
-		model.addAttribute("result", result);
-		response.addCookie(new Cookie("result", jsonMapper.toString(result)));
-		
-		UIData uiData = new UIData(exam, Language.valueOf(language), givenAnswers, result);
-		model.addAttribute("util", uiData);
-		
-		autoloadJS.injectAutoReloadJS(model);
-		return "result.html";
-	}
-	
-	
-	
-	@GetMapping("result-details.html")
-	public String resultDetails(Model model, @CookieValue("language") String language, @CookieValue(name = "givenAnswers", required = false) String givenAnswersJsonBase64, @CookieValue("result") String resultJsonBase64) {
-		List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJsonBase64);
-		
-		var result = jsonMapper.fromStringToCalculationResult(resultJsonBase64);
-		model.addAttribute("result", result);
-		
-		UIData uiData = new UIData(exam, Language.valueOf(language), givenAnswers, result);
-		model.addAttribute("util", uiData);
-		
-		autoloadJS.injectAutoReloadJS(model);
-		return "result-details.html";
-	}
-	
-	
-	
-	@GetMapping("end")
-	public String end(HttpServletResponse response, HttpServletRequest request) {
-		deleteCookies(response, request);
-		return "index.html";
-	}
+    private boolean answersMissing(Collection<TaskAnswer> givenAnswers) {
+        return givenAnswers.stream().filter(a -> !a.getOptionSelections().isEmpty()).count() < exam.getTasks().size();
+    }
 
-	private void deleteCookies(HttpServletResponse response, HttpServletRequest request) {
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null) {
-			for(Cookie cookie : cookies) {
-				// bandwidth
-				cookie.setValue("");
-				cookie.setMaxAge(0);
-				response.addCookie(cookie);
-			}
-		}
-	}
+    private Collection<TaskAnswer> parse(MultiValueMap<String, String> formData) {
+        if (formData == null) {
+            return Collections.emptyList();
+        }
+
+        Map<Integer, TaskAnswer> answers = new HashMap<Integer, TaskAnswer>();
+
+        for (Entry<String, List<String>> entry : formData.entrySet()) {
+            String[] parts = entry.getKey().split("-");
+            if ("frage".equals(parts[0])) {
+                int nr = Integer.parseInt(parts[1]);
+                String option = parts[2];
+                var answer = answers.computeIfAbsent(nr, x -> new TaskAnswer(x));
+                answer.put(option, entry.getValue());
+
+            }
+            if ("flag".equals(parts[0])) {
+                int nr = Integer.parseInt(parts[1]);
+                boolean flagged = entry.getValue().contains("on");
+                var answer = answers.computeIfAbsent(nr, x -> new TaskAnswer(x));
+                answer.setFlagged(flagged);
+            }
+        }
+
+        return answers.values();
+    }
+
+    @GetMapping("calculatePoints")
+    public String calculatePoints(HttpServletResponse response, Model model, @CookieValue("language") String language,
+            @CookieValue(name = "givenAnswers", required = false) String givenAnswersJsonBase64) {
+        List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJsonBase64);
+
+        Calculator calc = new Calculator();
+        var result = calc.calculate(exam, givenAnswers);
+        model.addAttribute("result", result);
+        response.addCookie(new Cookie("result", jsonMapper.toString(result)));
+
+        UIData uiData = new UIData(exam, Language.valueOf(language), givenAnswers, result);
+        model.addAttribute("util", uiData);
+
+        autoloadJS.injectAutoReloadJS(model);
+        return "result.html";
+    }
+
+    @GetMapping("result-details.html")
+    public String resultDetails(Model model, @CookieValue("language") String language,
+            @CookieValue(name = "givenAnswers", required = false) String givenAnswersJsonBase64,
+            @CookieValue("result") String resultJsonBase64) {
+        List<TaskAnswer> givenAnswers = givenAnswersFromCookie(givenAnswersJsonBase64);
+
+        var result = jsonMapper.fromStringToCalculationResult(resultJsonBase64);
+        model.addAttribute("result", result);
+
+        UIData uiData = new UIData(exam, Language.valueOf(language), givenAnswers, result);
+        model.addAttribute("util", uiData);
+
+        autoloadJS.injectAutoReloadJS(model);
+        return "result-details.html";
+    }
+
+    @GetMapping("end")
+    public String end(HttpServletResponse response, HttpServletRequest request) {
+        deleteCookies(response, request);
+        return "index.html";
+    }
+
+    private void deleteCookies(HttpServletResponse response, HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                // bandwidth
+                cookie.setValue("");
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+            }
+        }
+    }
 
 }
---
+++ src/test/java/org/isaqb/onlineexam/mockexam/DataConfigurationTest.java
@@ -0,0 +1,54 @@
+package org.isaqb.onlineexam.mockexam;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.isaqb.onlineexam.mockexam.model.Language;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
+@EnableConfigurationProperties(DataConfiguration.class)
+public class DataConfigurationTest {
+
+    @Autowired
+    private DataConfiguration config;
+
+    @Test
+    public void yamlInjected() {
+        assertNotNull(config);
+    }
+
+    @Test
+    public void simpleTasks() {
+        var ddd = config.getTasks().get("ddd");
+        assertEquals("Domain Driven Design", ddd.getName().get(Language.DE));
+        assertEquals("xx", ddd.getUrls().get(0).getUrlTemplate());
+        assertEquals(1, ddd.getUrls().get(0).getFrom());
+        assertEquals(2, ddd.getUrls().get(0).getTo());
+        assertEquals(2, ddd.getUrls().size());
+    }
+
+    @Test
+    public void resolveTaskRefs() {
+        var conf = config.getTasks().get("foundation");
+        assertTrue(conf.getRefs().isEmpty(), () -> conf.toString());
+        assertEquals(2, conf.getUrls().size(), () -> conf.toString());
+    }
+
+    @Test
+    public void examConfig() {
+        var conf = config.getExams().get("mock");
+        assertEquals("Foundation Level Mock Prüfung", conf.getName().get(Language.DE));
+        assertEquals(30, conf.getRequiredPoints());
+        assertEquals(60, conf.getMaxTimeInMinutes());
+        assertEquals("foundation", conf.getTaskRefs().get(0));
+    }
+}
