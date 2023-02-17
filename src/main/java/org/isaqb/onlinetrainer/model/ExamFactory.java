c6917928de061aa707ee27503455f6362919a230
Jan MatÃšrne <jan@materne.de>
2023-02-17 13:49:56 +0100

    organize import

--- src/main/java/org/isaqb/onlinetrainer/model/ExamFactory.java
+++ src/main/java/org/isaqb/onlinetrainer/model/ExamFactory.java
@@ -7,7 +7,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Random;
 import java.util.UUID;
 import java.util.function.Function;
