87b62bc2853ed88cf18004714593f2a9246b95aa
Jan Matèrne <jan@materne.de>
2024-01-05 05:28:52 +0100

    fix distribution script

--- create-distribution.bat
+++ create-distribution.bat
@@ -4,13 +4,15 @@ rem DATE  : dd.MM.yyyy
 rem TSTAMP: HH:MM:ss,SS
 rem           YYYY   .    MM     .     DD    _  HH       .    MM
 set TSTAMP=%DATE:~-4%.%DATE:~3,2%.%DATE:~0,2%_%TIME:~0,2%.%TIME:~3,2%
+rem replace leading spaces by zero by time field (before 10:00): " 8:30" becomes "08:30"
+set TSTAMP=%TSTAMP: =0%
 echo Nutze TSTAMP=%TSTAMP%
 echo APP_VERSION: %APP_VERSION%
 
 
-
 echo Clean
 rd /Q/S target
+
 
 :build
 echo Docker Build 
@@ -34,15 +36,14 @@ echo Publish Git
 git push
 
 echo Publish Image
-docker push janmaterne/onlinetrainer:latest 
+docker push janmaterne/onlinetrainer:latest
 docker push janmaterne/onlinetrainer:%TSTAMP%
 docker push janmaterne/onlinetrainer:%APP_VERSION%
 
 echo Erstelle GitHub Release mit Assets
 rem https://cli.github.com/manual/gh_release_create
-rem gh release create %APP_VERSION% --title "iSAQB OnlineTrainer %APP_VERSION% - %TSTAMP%" --notes "Version %APP_VERSION%" build\onlinetrainer-linux.tar.gz build\onlinetrainer-macos.tar.gz build\onlinetrainer-win32.zip build\libs\onlinetrainer-%APP_VERSION%.jar
-gh release create %APP_VERSION% --title "iSAQB OnlineTrainer %APP_VERSION% - %TSTAMP%" --notes-file src\changelog-%APP_VERSION%.adoc build\onlinetrainer-linux.tar.gz build\onlinetrainer-macos.tar.gz build\onlinetrainer-win32.zip build\libs\onlinetrainer-%APP_VERSION%.jar
-
+rem gh release create %APP_VERSION% --title "iSAQB OnlineTrainer %APP_VERSION% - %TSTAMP%" --notes-file src\changelog-%APP_VERSION%.adoc target\onlinetrainer-linux.tar.gz target\onlinetrainer-macos.tar.gz target\onlinetrainer-win32.zip target\onlinetrainer-%APP_VERSION%.jar
+gh release create %APP_VERSION% --title "iSAQB OnlineTrainer %APP_VERSION% - %TSTAMP%" --notes-file src\changelog-%APP_VERSION%.adoc target\onlinetrainer-linux.tar.gz target\onlinetrainer-macos.tar.gz target\onlinetrainer-win32.zip target\onlinetrainer-%APP_VERSION%.jar
 
 
 :end
